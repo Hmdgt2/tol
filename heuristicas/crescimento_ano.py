dc8bf9889ef9a0d11674f1f0f9d0a1d1e24ba53d
Hmdgt2 <hmdgit02@gmail.com>
2025-07-24 16:15:02 +0100

    Create crescimento_ano.py

---
+++ heuristicas/crescimento_ano.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from lib.dados import carregar_todos_sorteios
+
+def calcular_frequencia_por_ano(sorteios):
+    freq_ano = defaultdict(lambda: defaultdict(int))
+    for sorteio in sorteios:
+        ano = int(sorteio['data'].split('/')[-1])
+        for num in sorteio.get('numeros', []):
+            freq_ano[ano][num] += 1
+    return freq_ano
+
+def numeros_crescimento_ano(min_crescimentos=2):
+    sorteios = carregar_todos_sorteios()
+    freq_ano = calcular_frequencia_por_ano(sorteios)
+    anos = sorted(freq_ano.keys())
+
+    pontos = {}
+    for num in range(1, 50):
+        crescimentos = 0
+        for i in range(1, len(anos)):
+            freq_antes = freq_ano[anos[i - 1]].get(num, 0)
+            freq_depois = freq_ano[anos[i]].get(num, 0)
+            if freq_depois > freq_antes:
+                crescimentos += 1
+        if crescimentos >= min_crescimentos:
+            pontos[num] = crescimentos * 3
+    return pontos
+
+if __name__ == '__main__':
+    crescimento = numeros_crescimento_ano()
+    print("Números com crescimento anual significativo:")
+    for num, score in sorted(crescimento.items(), key=lambda x: x[1], reverse=True):
+        print(f"Número {num}: score {score}")
