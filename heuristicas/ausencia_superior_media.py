05a152b9e57cdf12e5d5814685753f4967222790
Hmdgt2 <hmdgit02@gmail.com>
2025-07-24 17:52:50 +0100

    Update ausencia_superior_media.py

--- heuristicas/ausencia_superior_media.py
+++ heuristicas/ausencia_superior_media.py
@@ -1,11 +1,12 @@
 # heuristicas/ausencia_superior_media.py
+from lib.dados import calcular_ausencia_atual
 
-from lib.dados import carregar_sorteios, calcular_ausencia_atual
-
-def ausencia_superior_media(n=2):
-    sorteios = carregar_sorteios()
+def prever(sorteios, n=2):
     ausencia = calcular_ausencia_atual(sorteios)
-
     media = sum(ausencia.values()) / len(ausencia)
     candidatos = [num for num, dias in ausencia.items() if dias > media]
-    return sorted(candidatos, key=lambda x: ausencia[x], reverse=True)[:n]
+    sugeridos = sorted(candidatos, key=lambda x: ausencia[x], reverse=True)[:n]
+    return {
+        "nome": "ausencia_superior_media",
+        "numeros": sugeridos
+    }
