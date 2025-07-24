19b4dd26522bb184ea357dd6437dadb9a8fbb628
Hmdgt2 <hmdgit02@gmail.com>
2025-07-24 17:54:16 +0100

    Update tendencia_recentes.py

--- heuristicas/tendencia_recentes.py
+++ heuristicas/tendencia_recentes.py
@@ -1,22 +1,13 @@
 # heuristicas/tendencia_recentes.py
+from lib.dados import contar_ocorrencias
 
-from collections import Counter
-from lib.dados import carregar_sorteios, contar_ocorrencias
-
-def tendencia_recentes(n=2, janela=10):
-    """
-    Retorna os n números mais frequentes nos últimos 'janela' concursos.
-    """
-    sorteios = carregar_sorteios()
-
+def prever(sorteios, n=2, janela=10):
     if len(sorteios) < janela:
         janela = len(sorteios)
-
     ultimos = sorteios[-janela:]
     contador = contar_ocorrencias(ultimos)
-
-    return [num for num, _ in contador.most_common(n)]
-
-
-if __name__ == "__main__":
-    print("Tendência recentes (últimos 10 concursos):", tendencia_recentes(2, 10))
+    sugeridos = [num for num, _ in contador.most_common(n)]
+    return {
+        "nome": "tendencia_recentes",
+        "numeros": sugeridos
+    }
