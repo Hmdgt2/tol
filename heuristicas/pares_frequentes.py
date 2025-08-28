d32d8beb99246163e6f5c24bbf2a6192db5567b1
Hmdgt2 <hmdgit02@gmail.com>
2025-08-28 13:10:23 +0100

    Update pares_frequentes.py

--- heuristicas/pares_frequentes.py
+++ heuristicas/pares_frequentes.py
@@ -1,7 +1,7 @@
 # heuristicas/pares_frequentes.py
 from collections import Counter
 
-def prever(estatisticas, n=2):
+def prever(estatisticas, n=5):
     """
     Prevê números com base na frequência de pares de números.
 
