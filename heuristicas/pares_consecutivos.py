f3f40bc379a9d740345691e74bbd81f009e10fec
Hmdgt2 <hmdgit02@gmail.com>
2025-08-28 11:44:13 +0100

    Update pares_consecutivos.py

--- heuristicas/pares_consecutivos.py
+++ heuristicas/pares_consecutivos.py
@@ -1,23 +1,41 @@
+# heuristicas/pares_consecutivos.py
 from collections import Counter
 
-def prever(sorteios, n=2):
+def prever(estatisticas, n=2):
+    """
+    Prevê números com base na frequência de pares de números consecutivos.
+    
+    Args:
+        estatisticas (dict): Dicionário com todas as estatísticas pré-calculadas.
+        n (int): O número de sugestões a retornar.
+        
+    Returns:
+        dict: Um dicionário com o nome da heurística e os números sugeridos.
+    """
+    pares_frequentes = estatisticas.get('pares_frequentes', {})
+    
+    if not pares_frequentes:
+        return {
+            "nome": "pares_consecutivos",
+            "numeros": []
+        }
+        
     contador_consecutivos = Counter()
-    for concurso in sorteios:
-        numeros = sorted(concurso.get('numeros', []))
-        # Identifica pares de números consecutivos
-        for i in range(len(numeros) - 1):
-            if numeros[i+1] - numeros[i] == 1:
-                contador_consecutivos.update([(numeros[i], numeros[i+1])])
-
-    pares_mais_frequentes = contador_consecutivos.most_common(5) # Top 5 pares
-
+    
+    # Filtra apenas os pares que são consecutivos
+    for par, freq in pares_frequentes.items():
+        if par[1] - par[0] == 1:
+            contador_consecutivos[par] = freq
+            
+    pares_mais_frequentes = contador_consecutivos.most_common(5)
+    
     contador_numeros = Counter()
     for par, _ in pares_mais_frequentes:
         contador_numeros.update(par)
-    
+        
     sugeridos = [num for num, _ in contador_numeros.most_common(n)]
-
+    
     return {
-        "heuristica": "pares_consecutivos",
-        "numeros": sorted(list(set(sugeridos)))[:n]
+        "nome": "pares_consecutivos",
+        "numeros": sorted(list(set(sugeridos)))
     }
