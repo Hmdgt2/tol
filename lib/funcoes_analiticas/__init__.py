32d13f445b770aed2f74daf799f3454521c0cac3
Hmdgt2 <hmdgit02@gmail.com>
2025-09-11 13:25:12 +0100

    Update __init__.py

--- lib/funcoes_analiticas/__init__.py
+++ lib/funcoes_analiticas/__init__.py
@@ -1,1 +1,7 @@
-
+# lib/funcoes_analiticas/__init__.py
+from .aritmetica import *
+from .estatisticas import *
+from .sequencias import *
+from .combinatoria import *
+from .conjuntos import *
+from .transformacoes import *
